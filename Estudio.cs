3eab5d4505828bb700bebb6f2cda7ac95242b547
maria luiza <91751375+maluzinhaa@users.noreply.github.com>
2022-05-18 21:29:01 -0300

    Add files via upload

---
+++ Estudio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao
+{
+    class Estudio
+    {
+        public string nome;
+        public string ceo;
+        public string sede;
+        public int fundacao;
+
+        public Estudio(string nome, string ceo, string sede, int fundacao)
+        {
+            this.nome = nome;
+            this.ceo = ceo;
+            this.sede = sede;
+            this.fundacao = fundacao;
+        }
+
+        public void ExibirInfo(int id)
+        {
+            Console.WriteLine($"Nome: {nome}\nCEO: {ceo}\nSede: {sede}\nFundação: {fundacao}\nID: {id} ");
+        }
+
+    }
+}
---
+++ Filme.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composicao
+{
+    class Filme
+    {
+        public string nome;
+        public int ano;
+        public float duracao;
+        public string genero;
+        public int classificacao;
+        public Estudio estudio;
+
+        public Filme(string nome, int ano, float duracao, string genero, int classificacao, Estudio estudio)
+        {
+            this.nome = nome;
+            this.ano = ano;
+            this.duracao = duracao;
+            this.genero = genero;
+            this.classificacao = classificacao;
+            this.estudio = estudio;
+        }
+
+        public void ExibirInfo()
+        {
+            Console.WriteLine($"Nome: {nome}\nAno: {ano}\nGênero: {genero}\nDuração: {duracao} horas\nClassificação: {classificacao}/10\nEstúdio: {estudio.nome}");
+            Console.Write("===================");
+        }
+
+    }
+}
